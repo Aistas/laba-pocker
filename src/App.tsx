ff544bb432a19cf8fcfe72607f6f42d9087f5d92
Aivar Bakirov <aivar2s1bakirov@gmail.com>
2024-05-22 12:48:28 +0600

    Minor changes

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,13 @@
-import './App.css'
+import './App.css';
+import Card from "./components/Cards/Card.tsx";
 
 const App = () => {
 
   return (
-    <>
-
-    </>
-  )
+      <div className="playingCards faceImages">
+        <Card rank={"K"} suit={"diams"}/>
+      </div>
+  );
 };
 
-export default App
+export default App;
