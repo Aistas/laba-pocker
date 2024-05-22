3dcd0230315f5890ffb69bbc51fce12944fddd74
Aivar Bakirov <aivar2s1bakirov@gmail.com>
2024-05-22 12:45:52 +0600

    Added Card.tsx

---
+++ src/components/Cards/Card.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+type Rank = '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9' | '10' | 'J' | 'Q' | 'K' | 'A';
+type Suit = 'diams' | 'hearts' | 'clubs' | 'spades';
+
+interface Props {
+    rank: Rank;
+    suit: Suit;
+}
+
+const Card: React.FC<Props> = ({ rank, suit }) => {
+    return (
+        <span className={`card rank-${rank} ${suit}`}>
+            <span className="rank">{rank}</span>
+            <span className="suit">{suit === 'diams' ? '♦' : suit === 'hearts' ? '♥' : suit === 'clubs' ? '♣' : '♠'}</span>
+        </span>
+    );
+};
+
+export default Card;
